165bdc5f94caed0bdbffe38453ce29f08cf0ad44
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-24 15:23:05 +0200

    Imported and configured the needed fonts for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{html,ts}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        aboreto: ['var(--font-aboreto)'],
+        alexBrush: ['var(--font-alex-brush)'],
+        afacad: ['var(--font-afacad)']
+      }
+    },
+    fontFamily: {
+      sans: ['var(--font-afacad)', 'sans-serif'],
+      serif: ['var(--font-afacad)', 'serif'],
+      mono: ['var(--font-afacad)', 'monospace'],
+      display: ['var(--font-afacad)'],
+      body: ['var(--font-afacad)']
+    }
   },
   plugins: []
 };
