4b3a313914ef2380ad91d17c973477aba2863fb6
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-31 18:37:15 +0200

    got rid of ununsed imports

--- src/app/layout/cards-carousel/cards-carousel.component.ts
+++ src/app/layout/cards-carousel/cards-carousel.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 import { ItemCardComponent } from '../../UI/cards/item-card/item-card.component';
-import {
-  LimitedAdditionCircleComponent
-} from '../../typography/limited-addition-circle/limited-addition-circle.component';
-import { ParagraphComponent } from '../../typography/paragraph/paragraph.component';
-import { StarIconComponent } from '../../UI/icons/star-icon/star-icon.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cards-carousel',
-  imports: [ItemCardComponent, LimitedAdditionCircleComponent, ParagraphComponent, StarIconComponent, RouterLink],
+  imports: [ItemCardComponent],
   standalone: true,
   templateUrl: './cards-carousel.component.html',
   styleUrl: './cards-carousel.component.css'
