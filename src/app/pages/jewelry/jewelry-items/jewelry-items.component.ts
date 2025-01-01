cfe46d012a22feed850aad48f9971aa80a75483d
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-01 14:56:42 +0200

    Parsed the real jewelry and inject it onto app-jewelry-items. Outsourced some reusable components to layout folder.

--- src/app/pages/jewelry/jewelry-items/jewelry-items.component.ts
+++ src/app/pages/jewelry/jewelry-items/jewelry-items.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ItemCardComponent } from '../../../UI/cards/item-card/item-card.component';
 import { ViajewelsButtonComponent } from '../../../UI/buttons/viajewels-button/viajewels-button.component';
+import { JewelryType } from '../../../../../data/JEWELRY';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-jewelry-items',
   imports: [
     ItemCardComponent,
-    ViajewelsButtonComponent
+    ViajewelsButtonComponent,
+    NgForOf,
+    NgIf
   ],
   standalone: true,
   templateUrl: './jewelry-items.component.html',
   styleUrl: './jewelry-items.component.css'
 })
 export class JewelryItemsComponent {
-
+  jewelry = input.required<JewelryType[]>();
 }
--- src/app/pages/jewelry/jewelry.component.ts
+++ src/app/pages/jewelry/jewelry.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SearchJewelryHeaderComponent } from './search-jewelry-header/search-jewelry-header.component';
 import { FilterJewelryComponent } from './filter-jewelry/filter-jewelry.component';
 import { JewelryItemsComponent } from './jewelry-items/jewelry-items.component';
 import { RecentSearchesComponent } from '../../layout/recent-searches/recent-searches.component';
+import { JewelryService } from '../../jewelry.service';
 
 @Component({
   selector: 'app-jewelry',
@@ -17,6 +18,12 @@ import { RecentSearchesComponent } from '../../layout/recent-searches/recent-sea
   styleUrl: './jewelry.component.css'
 })
 export class JewelryComponent implements OnInit {
+  private jewelryService = inject(JewelryService);
+
+
+  get jewelryItems() {
+    return this.jewelryService.getJewelry();
+  }
 
   ngOnInit() {
     // scroll to top
