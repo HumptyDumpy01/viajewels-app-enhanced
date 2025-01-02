1bdb5433da63fb263db361f4ac1909410751015e
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-02 20:46:42 +0200

    Added fallback text to jewel page regarding reviews section when there are no reviews yet/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavigationComponent } from './layout/navigation/navigation.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
@@ -10,5 +10,21 @@ import { FooterComponent } from './layout/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+
+  /* IMPORTANT: NEEDS CLOSE ATTENTION: BEHAVIOUR CAN BE UNPREDICTED
+  *   AND WORSEN USER EXPERIENCE*/
+  ngOnInit() {
+    // whenever the URL changes, scroll to the top of the page,
+    // this is required because when I click on e.g. some cards below when
+    // I browse the page, the page does not scroll to the top of the page though
+    // the data completely changed.
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
 }
--- src/app/pages/jewel/jewel-testimonials/jewel-testimonials.component.ts
+++ src/app/pages/jewel/jewel-testimonials/jewel-testimonials.component.ts
@@ -3,13 +3,15 @@ import { HeadingComponent } from '../../../typography/heading/heading.component'
 import { TestimonialsComponent } from '../../../layout/testimonials/testimonials.component';
 import { JewelryReviewType } from '../../../../../data/JEWELRY';
 import { BulkyBadgeComponent } from '../../../UI/badges/bulky-badge/bulky-badge.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-jewel-testimonials',
   imports: [
     HeadingComponent,
     TestimonialsComponent,
-    BulkyBadgeComponent
+    BulkyBadgeComponent,
+    NgIf
   ],
   standalone: true,
   templateUrl: './jewel-testimonials.component.html',
