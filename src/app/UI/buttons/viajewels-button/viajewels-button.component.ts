0518478c7dda2369d679236622996921bbb02487
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-02 19:25:41 +0200

    Added hover and active states to viajewels button component.

--- src/app/UI/buttons/viajewels-button/viajewels-button.component.ts
+++ src/app/UI/buttons/viajewels-button/viajewels-button.component.ts
@@ -16,9 +16,12 @@ export class ViajewelsButtonComponent {
   styling = input.required<ViajewelsButtonStylingType>();
 
   get getStyles() {
-    const defaultStyles = `border border-zinc-800`;
-    const whiteStyles = ` bg-white text-red-600`;
-    const emptyWhiteStyles = `text-white border border-white`;
+    const defaultStyles = `border border-zinc-800
+    transition-all duration-300 hover:bg-zinc-800 hover:text-white active:bg-white active:text-zinc-800`;
+    const whiteStyles = ` bg-white text-red-600 border border-red-600 transition-all duration-300 hover:bg-red-600 hover:text-white active:bg-white active:text-red-600`;
+    const emptyWhiteStyles = `text-white border border-white transition-all duration-300
+    hover:bg-white hover:text-red-600
+    active:bg-red-600 active:text-white`;
     return this.styling() === `default` ? defaultStyles : this.styling() === `white` ? whiteStyles : emptyWhiteStyles;
   }
 
