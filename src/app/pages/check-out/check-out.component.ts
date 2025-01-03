2ec66848bbde6f1596bd04e58acac8861a59ee76
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-03 14:11:35 +0200

    Removed unused imports.

--- src/app/pages/check-out/check-out.component.ts
+++ src/app/pages/check-out/check-out.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { ParagraphComponent } from '../../typography/paragraph/paragraph.component';
 import { CheckOutHeaderComponent } from './check-out-header/check-out-header.component';
 import { ShippingDetailsComponent } from './shipping-details/shipping-details.component';
 
 @Component({
   selector: 'app-checkout',
   imports: [
-    ParagraphComponent,
     CheckOutHeaderComponent,
     ShippingDetailsComponent
   ],
