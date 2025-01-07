6848024819f2a9819e3be8fd4e9b37f4347676e6
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-07 15:02:07 +0200

    Fixed issues with the wishlist item count not being updated.

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,13 @@
-import { Component, inject, input } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  effect,
+  inject,
+  Injector,
+  input,
+  OnInit,
+  runInInjectionContext
+} from '@angular/core';
 import { JewelWishlistService } from '../../jewel-wishlist.service';
 
 @Component({
@@ -8,11 +17,26 @@ import { JewelWishlistService } from '../../jewel-wishlist.service';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   private jewelWishlistService = inject(JewelWishlistService);
+  private cdr = inject(ChangeDetectorRef);
+  private injector = inject(Injector);
   heading = input(`ViaJewels`);
 
   cartItems = input(0);
-  wishlistedItems = this.jewelWishlistService.getWishlist().length;
+  wishlistedItems = 0;
 
+  ngOnInit() {
+    this.updateWishlistCount();
+    runInInjectionContext(this.injector, () => {
+      effect(() => {
+        this.updateWishlistCount();
+      }, { allowSignalWrites: true });
+    });
+  }
+
+  private updateWishlistCount() {
+    this.wishlistedItems = this.jewelWishlistService.getWishlist().length;
+    this.cdr.detectChanges();
+  }
 }
