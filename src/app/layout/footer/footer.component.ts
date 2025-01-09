7d9d2c419fbdab21aa16d5ce6e2a987b1942aab6
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-09 18:38:40 +0200

    Change the footer styling if user changes theme.

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InfoBadgeComponent } from '../../UI/badges/info-badge/info-badge.component';
 import { InstagramIconComponent } from '../../UI/icons/socials/instagram-icon/instagram-icon.component';
 import { LinkedinIconComponent } from '../../UI/icons/socials/linkedin-icon/linkedin-icon.component';
 import { ViberIconComponent } from '../../UI/icons/socials/viber-icon/viber-icon.component';
+import { ThemeService } from '../../theme.service';
+import { applyThemeClasses } from '../../../utils/functions/applyThemeClasses';
 
 @Component({
   selector: 'app-footer',
@@ -17,5 +19,11 @@ import { ViberIconComponent } from '../../UI/icons/socials/viber-icon/viber-icon
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  private themeService = inject(ThemeService);
 
+  get theme() {
+    return this.themeService.getTheme;
+  }
+
+  protected readonly applyThemeClasses = applyThemeClasses;
 }
